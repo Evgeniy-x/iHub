8aad565a5c249340f128f8eb2e25382d30b6ff3f
Evgeniy <yaremenko.x@gmail.com>
2024-03-10 23:28:02 +0200

    update router

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/i-hub">
+      <BrowserRouter basename="/test-iHub">
         <App />
       </BrowserRouter>
     </Provider>
